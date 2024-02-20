23c086ff90a84d0c05bcc2329d8e070c6c9de3b0
Carolina Ruiz <ing.acrd@gmail.com>
2024-02-20 10:14:05 -0500

    profile, edit and delete

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -9,13 +9,14 @@ export const POST = async (req) => {
         await connectToDB();
         const newPrompt = new Prompt({
             creator:userId, 
+            prompt,
             tag
         })
 
         await newPrompt.save();
         return new Response(JSON.stringify(newPrompt),{ status:201})
     } catch (error) {
-        
+        return new Response("Failed to create a new prompt", {status: 500})
     }
 
 }
--- components/Form.jsx
+++ components/Form.jsx
@@ -4,14 +4,14 @@ const Form = ({type, post, setPost, submitting, handleSubmit}) => {
   return (
     <section className="w-full max-w-full flex-start flex-col">
       <h1 className="head_text text_left">
-        <span className='blue_gradient'>{type}Post</span>
+        <span className='blue_gradient'>{type} Post</span>
       </h1>
       <p className="desc text-left max-w-md">{type} and share amazing promt s with the world, and let your imagination run wild with any AI-powered platform.
       </p>
-      <form 
-       onSubmit={handleSubmit}
-       className="mt-10 w-ful max-w-2x1 flex flex-col gap-7 glassmorphism "
-       >
+      <form
+        onSubmit={handleSubmit}
+        className='mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism'
+      >
         <label >
           <span className='font-satoshi font-semibold text-base text-gray-700'>
             Your AI Prompt
