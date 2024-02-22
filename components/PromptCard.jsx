dec342aa89ca2ec24777dde42770f17926ec9664
Carolina Ruiz <ing.acrd@gmail.com>
2024-02-22 12:04:01 -0500

    show other user's profile

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -19,10 +19,20 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
     setTimeout(()=> setCopied(""), 3000);
   }
 
+  const handleUserProfile = () => {
+    console.log(post);
+    if(post.creator._id === session?.user.id) return router.push("/profile");
+
+    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+  };
+
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
-        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+        <div 
+        className="flex-1 flex justify-start items-center gap-3 cursor-pointer"
+        onClick={handleUserProfile}
+        >
           <Image
           src={post.creator.image}
           alt="user_image"
@@ -31,8 +41,8 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
           className="rounded-full object-contain"
           />  
           <div className="flex flex-col">
-            <h3 >{post.creator.username}</h3>
-            <p >{post.creator.email}</p>
+            <h3 className='font-satoshi font-semibold text-gray-900'>{post.creator.username}</h3>
+            <p className="font-inter text-sm text-gray-500">{post.creator.email}</p>
           </div>
         </div>
 
@@ -43,6 +53,7 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
             ? '/assets/icons/tick.svg'
             : '/assets/icons/copy.svg'
           }
+          alt={copied === post.prompt? "tick_icon": "copy_icon"}
           width={12}
           height={12}
 
