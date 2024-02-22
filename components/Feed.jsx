1964eb0c7ba6555331280a21c70efc14bc94a288
Carolina Ruiz <ing.acrd@gmail.com>
2024-02-22 14:50:24 -0500

    fix feed function

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -25,18 +25,15 @@ const Feed = () => {
   const [searchTimeout, setSearchTimeout] = useState(null);
   const [searchedResults, setSearchedResults] = useState([]);
 
+  const fetchPosts = async() => {
+    const response = await fetch('/api/prompt');
+    const data = await response.json();
 
+    setPosts(data);
+  }
 
   useEffect(() => {
-    const fetchPosts = async() => {
-      const response = await fetch('/api/prompt');
-      const data = await response.json();
-
-      setPosts(data);
-    }
-
     fetchPosts();
-
   }, []);
 
   const filterPrompts = (searchtext) => {
